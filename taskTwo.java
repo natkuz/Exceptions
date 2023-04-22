24665efeb5488f926ca6829e17e44f3536206b02
NatalyaKuznetsova <nv8282@yandex.ru>
2023-04-22 13:58:22 +0800

    Changed solitions

--- taskTwo.java
+++ taskTwo.java
@@ -1,3 +1,12 @@
+// Если необходимо, исправьте данный код:
+// try {
+//     int d = 0;
+//     double catchedRes1 = intArray[8] / d;
+//     System.out.println("catchedRes1 = " + catchedRes1);
+//  } catch (ArithmeticException e) {
+//     System.out.println("Catching exception: " + e);
+//  }
+
 public class taskTwo {
     public static void main(String[] args) {
         int d = 0;
